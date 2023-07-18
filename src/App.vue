65e4adf10acbd2fcb4c69dd72093f7039b8372a8
sean-code <john.nganga@student.moringaschool.com>
2023-07-18 11:01:47 +0300

    Update: Render a new display of the pie chart

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ChartVue />
+
+  <!-- Uupdate Charts -->
+    <h1>My Pie Chart</h1>
+    <PieChart />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import ChartVue from "./components/ChartVue.vue";
+import PieChart from './components/PieChart.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
